084e6f06316516e379c1f2974783acd6584f9c08
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:48:36 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Totale speso? ");
+            double totale = double.Parse(Console.ReadLine());
+            double sconto;
+            double scontato;
+            if (totale <= 500)
+            {
+                sconto = totale / 100 * 10;
+                scontato = totale - sconto;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e l'importo da pagare è {scontato} euro");
+            }
+            else if (totale > 500)
+            {
+                sconto = totale / 100 * 20;
+                scontato = totale - sconto;
+                Console.WriteLine($"Lo sconto è di {sconto} euro e l'importo da pagare è {scontato} euro");
+            }
+            Console.ReadLine();
+        }
+    }
+}
